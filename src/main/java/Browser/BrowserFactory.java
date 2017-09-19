758cd9d5a7d351c60824585d796a6f9ea2c4e265
Yadav <syadav12@ic.ac.uk>
2017-09-19 11:55:19 +0100

    First gurru99 project commit, includes BrowserFactory and Test files

---
+++ src/main/java/Browser/BrowserFactory.java
@@ -0,0 +1,34 @@
+package Browser;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class BrowserFactory {
+	
+	private static WebDriver driver;
+	
+	public static WebDriver getBrowser(String browser){
+		if(driver!=null)return driver;
+		
+		if(browser.equalsIgnoreCase("Chrome")){
+			System.setProperty("webdriver.chrome.driver", "src"+File.separator+"BrowserDriver"+File.separator+"chromedriver.exe");
+			driver = new ChromeDriver();
+		}else if(browser.equalsIgnoreCase("FireFox")){
+			driver = new FirefoxDriver();
+		}else if(browser.equalsIgnoreCase("InternetExplorer")){
+			driver = new InternetExplorerDriver();
+		}
+		
+		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(200, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		
+		return driver;
+	}
+
+}
---
+++ src/test/java/tests/mainTest.java
@@ -0,0 +1,17 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+
+import Browser.BrowserFactory;
+
+public class mainTest {
+
+	public static void main(String[] args) {
+		WebDriver driver = null;
+
+		driver =  BrowserFactory.getBrowser("Chrome");
+		driver.get("http://www.bbc.co.uk");
+
+	}
+
+}
