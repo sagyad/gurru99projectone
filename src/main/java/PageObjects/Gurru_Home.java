2e79da7d12b02bb9f0f8e700bb76b0244da5b0f8
Sagar Yadav <svyadav@gmail.com>
2017-09-22 15:01:24 +0100

    added setExcelFile method

--- src/main/java/PageObjects/Gurru_Home.java
+++ src/main/java/PageObjects/Gurru_Home.java
@@ -1,5 +1,8 @@
 package pageObjects;
 
+import java.io.FileInputStream;
+
+import org.apache.poi.xssf.usermodel.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -7,18 +10,21 @@ import org.openqa.selenium.support.PageFactory;
 
 import utility.Constant;
 
-/*
+/**
  * Declare some common parameters for scripts
  * You can change them to adapt your environment
  *  21/09/2017 - added Constant class with all constant variables declared.
+ *  22-09-2017 - added all Excel variables
  */
 public class Gurru_Home {
 	
-
-	//--- Test ICL_workingCopy
+	private static XSSFSheet excelWSheet;
+	private static XSSFWorkbook excelWBook;
+	private static XSSFCell excelWCell;
+	private static XSSFRow excelWRow;
 	
 	Constant con = new Constant();
-	private WebDriver driver;
+	private static WebDriver driver;
 
 	@FindBy(xpath = ".//input[@name='uid']")
 	private WebElement userName;
@@ -34,6 +40,32 @@ public class Gurru_Home {
 		PageFactory.initElements(this.driver, this);
 	}
 
+	
+	//This method is to set the file path and to open excel file, pass excel path and sheetname as argument to this method
+	
+	public void setExcelFile(String path, String sheetName) throws Exception{
+		try{
+			//Open the excel file
+			FileInputStream excelFile = new FileInputStream(path);
+			
+			//Access the required test data sheet
+			excelWBook = new XSSFWorkbook(excelFile);
+			excelWSheet = excelWBook.getSheet(sheetName);
+		}catch(Exception e){
+			throw(e);
+		}
+	}
+	
+//	public void String setCellData(String rowNum, int colNum) throws Exception{
+//		try{
+//			
+//			return rowNum;
+//		}catch(Exception e){
+//			return "";
+//		}
+//	}
+	
+	
 	public void verifyGurru99BankHomePage() {
 		driver.get(Constant.URL);
 		System.out.println();
@@ -62,5 +94,5 @@ public class Gurru_Home {
 					"Cannot verify page title - " + driver.getTitle() + " - check your internet connect is working");
 		}
 	}
-	
+
 }
--- src/main/java/utility/Constant.java
+++ src/main/java/utility/Constant.java
@@ -1,9 +1,11 @@
 package utility;
 
+import java.io.File;
 
 /**
  * @author = Sagar Yadav
  * 		- Store all constant variables  
+ * 		- 22-09-2017 added  Excel File path and file name
  */
 public class Constant {
 	
@@ -12,5 +14,8 @@ public class Constant {
 	public static final String PASSWORD ="buqusus";
 	public static final String HOME_PAGE_EXPECT_TITLE = "Guru99 Bank Home Page";
 	public static final String AFTER_LOGIN_EXPECT_TITLE = "Guru99 Bank Manager HomePage";
+	
+	public static final String PATH_TESTDATA = "src"+File.separator+"main"+File.separator+"java"+File.separator+"testData"+File.separator;
+	public static final String FILE_TESTDATA ="testData.xls";
 
 }
